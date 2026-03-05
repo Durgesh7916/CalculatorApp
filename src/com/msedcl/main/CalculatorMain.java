91c48afb7d46b03d07cd66f5dfa39ecf304a788f
student13 <student13@host.docker.internal>
2026-03-05 10:48:41 +0530

    initial commit with calculatorutil  and calculatormain class

---
+++ src/com/msedcl/main/CalculatorMain.java
@@ -0,0 +1,15 @@
+package com.msedcl.main;
+
+import com.msedcl.main.util.CalculatorUtil;
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CalculatorUtil  calutil=new CalculatorUtil();
+		System.out.println("Addition="+calutil.addition(10,20));
+
+	}
+
+}
