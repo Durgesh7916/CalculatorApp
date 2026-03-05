0e226382079fb53e1e67d9c66165eb366ccfba93
student7 <student7@PC7.mshome.net>
2026-03-05 12:27:32 +0530

    conflic solved

--- src/com/msedcl/main/util/CalculatorUtil.java
+++ src/com/msedcl/main/util/CalculatorUtil.java
@@ -4,12 +4,9 @@ public class CalculatorUtil {
 	public int addition(int number1,int number2)
 	{
 		
-<<<<<<< HEAD
-		return number1+number2+0;
-=======
+
 		System.out.println("Adding two number");
 		return number1+number2;
->>>>>>> branch 'master' of https://github.com/Durgesh7916/CalculatorApp.git
 	}
 
 	
